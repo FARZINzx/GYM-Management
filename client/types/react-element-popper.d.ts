79509099db6b578b9b6ec604641265290371db6f
Farzin Hamzehi <5farzinhamzei@gmail.com>
2025-04-30 21:54:33 +0330

    fix error

--- client/types/react-element-popper.d.ts
+++ client/types/react-element-popper.d.ts
@@ -7,3 +7,20 @@ declare module 'react-element-popper/animations/transition' {
     const animation: any;
     export default animation;
 }
+
+declare module 'react-date-object/calendars/persian' {
+    const calendar: any;
+    export default calendar;
+  }
+  
+  declare module 'react-date-object/locales/persian_fa' {
+    const locale: any;
+    export default locale;
+  }
+  
+  declare module 'react-multi-date-picker' {
+    import { FC } from 'react';
+    const DatePicker: FC<any>;
+    export default DatePicker;
+  }
+  
