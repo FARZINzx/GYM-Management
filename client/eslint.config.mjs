8664657d960582035f52a5540cde13cd00fd9d73
FARZINzx <5farzinhamzei@gmail.com>
2025-06-08 08:33:52 +0330

    - edit eslint rules - delete unused variables

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,20 +1,22 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+import {FlatCompat} from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
     ...compat.config({
-      extends : ["next/core-web-vitals", "next/typescript"],
-      rules: {
-        "@typescript-eslint/no-explicit-any": "off"
-      }
+        extends: ["next/core-web-vitals", "next/typescript"],
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off",
+            "@react-hooks/exhaustive-deps": "off"
+
+        }
     })
 ];
 
